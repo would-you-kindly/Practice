21e127b9e299656e31d05c9d7d0d4273b089ebf9
OlegMatvienko <omatvienko@ics.perm.ru>
2017-06-01 22:31:18 +0500

    Добавил проект с тестами. Отрефакторил метод парсинга параметров для покрытия тестами. Добавил комментарии с замечаниями.

--- RemoveFilesTests/Properties/AssemblyInfo.cs
+++ RemoveFilesTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RemoveFiles")]
+[assembly: AssemblyTitle("RemoveFilesTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("RemoveFiles")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RemoveFilesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55c82b4f-9c01-4658-8295-a523127b04c7")]
+[assembly: Guid("dfb8b6a4-cd6c-4099-99d6-7bca844b0232")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("RemoveFilesTests")]
